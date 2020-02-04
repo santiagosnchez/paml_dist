09d6fbae5593b24f10d40f557e54da41c4f279b0
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2020-02-04 15:59:24 -0500

    added PamlDist.py

---
+++ PamlDist.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from multiprocessing import Pool, Lock, Process
+
+def cmd_constructor():
+    ctl = """
+    seqfile = infile
+    outfile = outfile
+    verbose = 0
+    icode = 0
+    weighting = 1
+    commonf3x4 = 0
+    """
+    cmd = "echo \'" + ctl + "\'" + " | yn00 /dev/stdin &> /dev/null"
+    return(cmd)
+
+sd
+
+def fix_codons(seq):
+    for i in range(0,len(seq),3):
+        if "N" in seq[i:i+3]:
+            seq = seq[:i] + "---" + seq[(i+3):]
+        if "-" in seq[i:i+3]:
+            seq = seq[:i] + "---" + seq[(i+3):]
+        if "TGA" == seq[i:i+3] or "TAA" == seq[i:i+3] or "TAG" == seq[i:i+3]:
+            seq = seq[:i] + "---" + seq[(i+3):]
+    return(seq)
+
+def retrieve_from_output():
+    with open("2YN.dN","r") as f:
+        dn = f.readlines()
+        dn = dn[2].split(" ")[-1]
+    with open("2YN.dS","r") as f:
+        ds = f.readlines()
+        ds = ds[2].split(" ")[-1]
+    return(dn,ds)
+
+def single_output_to_screen(lock, seq1, seq2, dn, ds):
+     lock.acquire()
+     try:
+         print(seq1, seq2, dn, ds, sep=",")
+     finally:
+         lock.release()
+
+def masterfn(seqs):
+    pid = os.getpid()
+    dirname = "tmp."+pid
+    os.makedirs(dirname)
+    os.chdir(dirname)
+    for i in range(1,len(seqs),2): seqs[i] = fix_codons(seqs[i].rstrip()) + "\n"
+    with open("inputfile","w") as f:
+        for i in seqs: f.write(i)
+    cmd = cmd_constructor()
+    sig = os.system(cmd)
+    if sig == 0:
+        dn,ds = retrieve_from_output()
+        seq1 = seqs[0][1:].rstrip()
+        seq2 = seqs[2][1:].rstrip()
+    os.chdir("../")
+    os.rmdir(dirname)
